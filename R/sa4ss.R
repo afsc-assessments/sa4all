2bb3b6b69759fb1c4313cdd1d945b7909ec681b7
jimianelli <jim.ianelli@gmail.com>
2021-05-11 22:41:00 -0700

    renamed file

--- R/sa4ss.R
+++
@@ -1,9 +0,0 @@
-#' sa4all: Generate stock assessment pdfs from Stock Synthesis (SS)
-#' 
-#' The \pkg{sa4all} is designed to facilitate the easy construction of
-#' stock assessment documents for species that were modelled with
-#' Stock Synthesis (SS), a statistical catch-at-age assessment framework.
-#' 
-#' @docType package
-#' @name sa4all
-NULL
